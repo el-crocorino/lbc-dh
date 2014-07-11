bf04a415c1198086e43ac5c9748d96e614bf6d60
olivier <owenzek@gmail.com>
2014-07-11 21:30:49 +0200

    LBC #11 - Constrqint & Comments added

--- sql/2014-11.sql
+++ sql/2014-11.sql
@@ -1,3 +1,5 @@
+--2014-07-11 - create searches table
+
 DROP TABLE IF EXISTS `search`;
 CREATE TABLE IF NOT EXISTS `search` (
   `search_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -6,5 +8,12 @@ CREATE TABLE IF NOT EXISTS `search` (
   `search_title` varchar(150) NOT NULL,
   `search_created` datetime NOT NULL,
   `search_updated` datetime NOT NULL,
-  PRIMARY KEY (`search_id`)
+  PRIMARY KEY (`search_id`),
+  CONSTRAINT fk_user_id
+   FOREIGN KEY (search_user_id)
+   REFERENCES user(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+#ALTER TABLE search
+#ADD CONSTRAINT fk_user_id FOREIGN KEY (search_user_id) REFERENCES user(user_id);
