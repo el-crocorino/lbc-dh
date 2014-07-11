ddcfba15b0061627f5b11d5c9157ff9ac2e5a9ca
olivier <owenzek@gmail.com>
2014-07-11 21:07:05 +0200

    LBC #8 - Login error messages

--- www/views/view_error_message.php
+++ www/views/view_error_message.php
@@ -1,3 +1,5 @@
 <div class="error_message">
-    <span><?php echo isset($dDisplay['error']) ? $dDisplay['error']['message']: '' ; ?></span>
+    <span>
+        <?php echo isset($dDisplay['error']) ? $dDisplay['error']['message']: '' ; ?>
+    </span>
 </div>
