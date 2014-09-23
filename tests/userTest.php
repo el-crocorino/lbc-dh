0297b536abc46a8f5e4485420dd940d4dc3f017c
olivier <owenzek@gmail.com>
2014-09-23 22:49:39 +0200

    LBC-09 - user Test updated

--- tests/userTest.php
+++ tests/userTest.php
@@ -1,5 +1,11 @@
 <?php
 
+    
+    require_once('../includes/functions.php');
+    require_once('../includes/load.php');
+
+    require_once('../classes/basic/basic.class.php');
+    require_once('../classes/user/user_orm.class.php');
     require_once('../classes/user/user.class.php');
 
     class userTest extends PHPUnit_Framework_TestCase {
@@ -8,9 +14,6 @@
 
         public function setUp() {
 
-        }
-
-        public function testCreateNewUser() {
             $data = array(
                 'id' => '1',
                 'username' => 'Toto',
@@ -21,7 +24,19 @@
                 'created' => '2014-03-17',
                 'updated' => '2014-06-17'
                 );
+            
             $this->user = new user($data);
+
+        }
+
+        public function testPasswordVerify() {
+
+            $this->user->hash_password();
+            $this->assertTrue($this->user->check_password('tata', $this->user->get_password()));
+
+        }
+
+        public function testToArray() {
 
         }
 
